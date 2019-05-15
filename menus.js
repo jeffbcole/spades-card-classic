85118e7834814120ffe444c2add9ed24336487a7
JeffCole Mac <jeffbcolemail@gmail.com>
2019-05-15 08:16:52 -0700

    Fixes
    Fixes

--- menus.js
+++ menus.js
@@ -358,7 +358,7 @@ function play_more_games_close_click() {
 	var el = document.getElementById('play_more_games_menu');
 	with(el.style) {
 		WebkitTransition = MozTransition = OTransition = msTransition = "0.4s ease-in";
-		bottom = "-200pt";
+		bottom = "-260px";
 		opacity = 0;
 		pointerEvents = "none";
 	}
